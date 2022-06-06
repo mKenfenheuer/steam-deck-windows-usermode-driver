de2e20c34899467a5de85f9b75d1af411d345b52
Maximilian Kenfenheuer <maximilian.kenfenheuer@ksol.it>
2022-06-06 21:37:22 +0200

    Navigation

--- SWICD/ViewModels/MainWindowViewModel.cs
+++ SWICD/ViewModels/MainWindowViewModel.cs
@@ -1,16 +1,29 @@
 ﻿using SWICD.Model;
+using SWICD.Pages;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace SWICD.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<NavigationItemModel> NavigationItems { get; set; }
+        public NavigationItemModel SelectedNavigationItem
+        {
+            set
+            {
+                _ = OnNavigationItemSelected(value);
+                NotifyPropertyChanged(nameof(SelectedNavigationItem));
+            }
+            get => null;
+        }
+        public Page ContentPage { get; set; }
 
         public MainWindowViewModel()
         {
@@ -19,6 +32,23 @@ namespace SWICD.ViewModels
             {
                 Title = "Settings"
             });
+            NavigationItems.Add(new NavigationItemModel()
+            {
+                Title = "Profiles"
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public async Task OnNavigationItemSelected(NavigationItemModel item)
+        {
+            ContentPage = new SettingsPage();
+            NotifyPropertyChanged(nameof(ContentPage));
+        }
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
         }
     }
 }
